aceab181f43869ebe7937aeed25da458f11a59e2
MuriloDutraProjects <MuriloDutraProjects@gmail.com>
2021-01-18 15:51:52 -0300

    New component - <Footer />

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Footer.scss'
+
+
+function Footer(props){
+    return (
+        <div className="links-container">
+            <ul className="links">
+                <li>Você sabe quem ele é?</li>
+                <li>Men band</li>
+                <li>Boy band</li>
+                <li>Tour</li>
+            </ul>
+        </div>
+    )
+}
+
+
+export default Footer
