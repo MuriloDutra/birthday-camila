4cb7c072ae5eb55494592d2d293d8f57b2600dd1
MuriloDutraProjects <MuriloDutraProjects@gmail.com>
2021-01-20 15:48:17 -0300

    New constant - partyImages

---
+++ src/constants/partyImages.js
@@ -0,0 +1,21 @@
+const partyImages = [
+    {
+        imageUrl: require('../assets/partyImages/harry01.jpg').default,
+        description: 'Harry jovem'
+    },
+    {
+        imageUrl: require('../assets/partyImages/harry02.webp').default,
+        description: 'Harry com a crush'
+    },
+    {
+        imageUrl: require('../assets/partyImages/harry03.jpg').default,
+        description: 'Harry com um drink'
+    },
+    {
+        imageUrl: require('../assets/partyImages/harry04.jpg').default,
+        description: 'Harry com uniforme de beisebol'
+    }
+]
+
+
+export default partyImages 
