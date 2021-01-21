8cd664525f21556d9f59e4b95a6d0875afa33075
MuriloDutraProjects <MuriloDutraProjects@gmail.com>
2021-01-21 14:51:33 -0300

    New constants

---
+++ src/constants/catalogImages.js
@@ -0,0 +1,35 @@
+const catalogImages = [
+    {
+        imageUrl: require('../assets/catalogImages/harry01.jpg').default,
+        portugueseDescription: 'Harry palestrando',
+        englishDescription: 'Harry speaking'
+    },
+    {
+        imageUrl: require('../assets/catalogImages/harry02.jpg').default,
+        portugueseDescription: 'Harry cowboy',
+        englishDescription: 'Country boy'
+    },
+    {
+        imageUrl: require('../assets/catalogImages/harry03.jpg').default,
+        portugueseDescription: 'Harry na aglomeração',
+        englishDescription: `Harry does not respect the quarantine`
+    },
+    {
+        imageUrl: require('../assets/catalogImages/harry04.png').default,
+        portugueseDescription: 'Harry com um desconhecido',
+        englishDescription: 'Harry with a stranger'
+    },
+    {
+        imageUrl: require('../assets/catalogImages/harry05.jpg').default,
+        portugueseDescription: 'Harry com a Taylor Swift',
+        englishDescription: 'Harry with a bad bitch'
+    },
+    {
+        imageUrl: require('../assets/catalogImages/harry06.jpg').default,
+        portugueseDescription: 'Harry com um cabelo e óculos top',
+        englishDescription: 'Great hair and glasses'
+    }
+]
+
+
+export default catalogImages 
---
+++ src/constants/slideImages.js
@@ -0,0 +1,25 @@
+const partyImages = [
+    {
+        imageUrl: require('../assets/slideImages/harry01.jpg').default,
+        portugueseDescription: 'Harry jovem',
+        englishDescription: 'Young Harry'
+    },
+    {
+        imageUrl: require('../assets/slideImages/harry02.webp').default,
+        portugueseDescription: 'Harry com a crush',
+        englishDescription: 'Harry in love'
+    },
+    {
+        imageUrl: require('../assets/slideImages/harry03.jpg').default,
+        portugueseDescription: 'Harry com um drink',
+        englishDescription: 'Harry drinking something'
+    },
+    {
+        imageUrl: require('../assets/slideImages/harry04.jpg').default,
+        portugueseDescription: 'Harry com uniforme de beisebol',
+        englishDescription: 'Harry wearing a baseball uniform'
+    }
+]
+
+
+export default partyImages 
