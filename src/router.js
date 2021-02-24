2e25f03c7fbf26c4cb7e5ab4244e6100c40f50b1
Murilo Dutra Alves <murilo.dutra2000@gmail.com>
2021-02-24 16:13:27 -0300

    New global function - toggleFeedback()

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Header from './components/header/Header'
+import Feedback from './components/feedback/Feedback'
+
 import Admin from './pages/admin/Admin'
 import ErrorPage from './pages/errorPage/ErrorPage'
 import Home from './pages/home/Home'
@@ -8,22 +10,45 @@ import Tour from './pages/tour/Tour'
 
 
 function Router(props){
+    const [showMessage, setShowMessage] = useState(false)
+    const [isErrorMessage, setIsErrorMessage] = useState(false)
+    const [message, setMessage] = useState('')
+    var timeout = null
+
+
+    async function toggleFeedback(isNegativeMessage, text){
+        if(showMessage){
+            return
+        }
+
+        setIsErrorMessage(isNegativeMessage)//true === Negative message
+        setMessage(text)
+        setShowMessage(true)
+
+        timeout = setTimeout(() => {
+            setShowMessage(false)
+            clearTimeout(timeout)
+        }, 6000)
+    }
+
+
     return (
         <>
             <Header />
+            <Feedback showMessage={showMessage} isErrorMessage={isErrorMessage} message={message} />
 
             <BrowserRouter>
                 <Switch>
                     <Route exact path="/">
-                        <Home />
+                        <Home toggleFeedback={toggleFeedback} />
                     </Route>
 
                     <Route exact path="/photos">
-                        <Tour />
+                        <Tour toggleFeedback={toggleFeedback} />
                     </Route>
 
                     <Route exact path="/admin">
-                        <Admin />
+                        <Admin toggleFeedback={toggleFeedback} />
                     </Route>
 
                     <Route path="*">
